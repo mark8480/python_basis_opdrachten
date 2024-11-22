34e217569b1a44fbb6d37947cd2ec36076ee4055
mark8480 <97069699@st.deltion.nl>
2024-11-22 12:40:14 +0100

    Add files via upload

--- opdrachten/010_variabelen/opdr_3/opdr_3.py
+++ opdrachten/010_variabelen/opdr_3/opdr_3.py
@@ -1,9 +1,23 @@
-# Opdracht 1
+# Opdracht 3 tekstfuncties
 # Naam student: Mark Heite
 # Groep:
 
 # Hier komt je code...
 
-my_dict = {"naam": "willem", "achternaam": "van der broek", "leeftijd": "23", "favoriete kleur": "blauw"}
 
-print(my_dict)
+a = "\t    *\t"
+b = "\t   ***\t"
+c = "\t  ******"
+d = "\t ********"
+e = "\t***********"
+f = "\t    ***"
+g= "\t    ***"
+h = "\t    ***"
+print((a + " ") * 5)
+print((b + " ") * 5)
+print((c + " ") * 5)
+print((d + " ") * 5)
+print((e + " ") * 5)
+print((f+ " ") * 5)
+print((g + " ") * 5)
+print((h + " ") * 5)
