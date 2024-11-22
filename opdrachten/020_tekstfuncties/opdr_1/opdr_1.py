a0133aa689e075cc8eb8f71f9667d59e62456ae4
mark8480 <97069699@st.deltion.nl>
2024-11-22 11:09:25 +0100

    Add files via upload

--- opdrachten/020_tekstfuncties/opdr_1/opdr_1.py
+++ opdrachten/020_tekstfuncties/opdr_1/opdr_1.py
@@ -1,5 +1,5 @@
 # Opdracht 1
-# Naam student:
+# Naam student: Mark heite
 # Groep:
 
 # Hier komt je code...
@@ -8,12 +8,13 @@
 # Zorg dat de volgende zin op het scherm wordt getoond met de print-functie:
 # O mamma mia, wat maak je toch heerlijke pizza's
 
-print()
+print("O mamma mia, wat maak je toch heerlijke pizza's")
 
 # Gebruik onderstaande variabelen en de print-functie
 # en toon de volgende zin op het scherm: Albert Einstein zei ooit: 'Als je niks fout doet, dan kun je ook niks leren!'
 voornaam = "albert"
 achternaam = "einstein"
 
+fullname = f"{voornaam} {achternaam}"
+print(fullname.title() + " zei ooit: 'Als je niks fout doet, dan kun je ook niks leren!'")
 
-print()
