1a1119b8d2cf33107e33e37e9c7ce8e5c5de2103
mark8480 <97069699@st.deltion.nl>
2024-11-22 12:43:23 +0100

    Add files via upload

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -1,5 +1,23 @@
 # Opdracht 3 tekstfuncties
-# Naam student:
+# Naam student: Mark Heite
 # Groep:
 
 # Hier komt je code...
+
+
+a = "\t    *\t"
+b = "\t   ***\t"
+c = "\t  ******"
+d = "\t ********"
+e = "\t***********"
+f = "\t    ***"
+g= "\t    ***"
+h = "\t    ***"
+print((a + " ") * 5)
+print((b + " ") * 5)
+print((c + " ") * 5)
+print((d + " ") * 5)
+print((e + " ") * 5)
+print((f+ " ") * 5)
+print((g + " ") * 5)
+print((h + " ") * 5)
