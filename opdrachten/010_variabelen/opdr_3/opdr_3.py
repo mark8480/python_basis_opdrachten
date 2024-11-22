f2e92378a09323b67c9b91e0264c68bf98233042
mark8480 <97069699@st.deltion.nl>
2024-11-22 10:56:08 +0100

    Add files via upload

--- opdrachten/010_variabelen/opdr_3/opdr_3.py
+++ opdrachten/010_variabelen/opdr_3/opdr_3.py
@@ -1,9 +1,9 @@
 # Opdracht 1
-# Naam student:
+# Naam student: Mark Heite
 # Groep:
 
 # Hier komt je code...
 
-my_dict = ...
+my_dict = {"naam": "willem", "achternaam": "van der broek", "leeftijd": "23", "favoriete kleur": "blauw"}
 
-print()
+print(my_dict)
